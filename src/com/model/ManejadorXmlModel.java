afda0908ca94b63f51d7617eb952251cfe46b58d
cgomezmendez <cjgm23@gmail.com>
2012-11-27 04:58:30 +0000

    problemas para guardar en java desde el xml

--- src/com/model/ManejadorXmlModel.java
+++ src/com/model/ManejadorXmlModel.java
@@ -4,6 +4,7 @@
  */
 package com.model;
 
+import java.util.HashMap;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -12,12 +13,53 @@ import org.xml.sax.helpers.DefaultHandler;
  * @author cgomezmendez
  */
 public class ManejadorXmlModel extends DefaultHandler{
-
+    private String tituloCancion;
+    private String nombreArtista;
     @Override
     public void startDocument() throws SAXException {
         super.startDocument();
         System.out.println("inicio");
     }
-    
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        if (attributes.getLocalName(0).equalsIgnoreCase("title")){
+            setNombreArtista(attributes.getValue(0));
+        }
+        if (attributes.getLocalName(0).equalsIgnoreCase("name")){
+            setTituloCancion(attributes.getValue(0));
+        }
+    }
+
+    /**
+     * @return the tituloCancion
+     */
+    public String getTituloCancion() {
+        return tituloCancion;
+    }
+
+    /**
+     * @param tituloCancion the tituloCancion to set
+     */
+    public void setTituloCancion(String tituloCancion) {
+        this.tituloCancion = tituloCancion;
+    }
+
+    /**
+     * @return the nombreArtista
+     */
+    public String getNombreArtista() {
+        return nombreArtista;
+    }
+
+    /**
+     * @param nombreArtista the nombreArtista to set
+     */
+    public void setNombreArtista(String nombreArtista) {
+        this.nombreArtista = nombreArtista;
+    }
+
+
  
 }
