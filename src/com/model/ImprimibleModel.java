fce40c52b49c641f9817908b483b183a8f200fb9
cgomezmendez <cjgm23@gmail.com>
2012-11-27 02:53:51 +0000

    interfaz para los datos de entrada que seran imprimibles a twitter

---
+++ src/com/model/ImprimibleModel.java
@@ -0,0 +1,13 @@
+/*
+ * 
+ * 
+ */
+package com.model;
+import java.util.ArrayList;
+/**
+ *
+ * @author cgomezmendez
+ */
+public interface ImprimibleModel {
+    public ArrayList obtenerInfo();
+}
