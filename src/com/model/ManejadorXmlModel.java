b58580e1d318cdb1e0fada666cfec557243665fd
cgomezmendez <cjgm23@gmail.com>
2012-11-27 04:06:59 +0000

    Reescribiendo el manejador

--- src/com/model/ManejadorXmlModel.java
+++ src/com/model/ManejadorXmlModel.java
@@ -5,68 +5,19 @@
 package com.model;
 
 import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 /**
  *
  * @author cgomezmendez
  */
 public class ManejadorXmlModel extends DefaultHandler{
-        public void startDocument ()
-    {
-	System.out.println("Inicio Documento");
+
+    @Override
+    public void startDocument() throws SAXException {
+        super.startDocument();
+        System.out.println("inicio");
     }
-
-
-    public void endDocument ()
-    {
-	System.out.println("Final Documento");
-    }
-
-
-    public void startElement (String uri, String name,
-			      String qName, Attributes atts)
-    {
-	if ("".equals (uri))
-	    System.out.println("Elemento Inicial: " + qName);
-	else
-	    System.out.println("Elemento Inicial: {" + uri + "}" + name);
-    }
-
-
-    public void endElement (String uri, String name, String qName)
-    {
-	if ("".equals (uri))
-	    System.out.println("Elemento Final: " + qName);
-	else
-	    System.out.println("Elemento Final:   {" + uri + "}" + name);
-    }
-
-
-    public void characters (char ch[], int start, int length)
-    {
-	System.out.print("Characters:    \"");
-	for (int i = start; i < start + length; i++) {
-	    switch (ch[i]) {
-	    case '\\':
-		System.out.print("\\\\");
-		break;
-	    case '"':
-		System.out.print("\\\"");
-		break;
-	    case '\n':
-		System.out.print("\\n");
-		break;
-	    case '\r':
-		System.out.print("\\r");
-		break;
-	    case '\t':
-		System.out.print("\\t");
-		break;
-	    default:
-		System.out.print(ch[i]);
-		break;
-	    }
-	}
-	System.out.print("\"\n");
-    }
+    
+ 
 }
