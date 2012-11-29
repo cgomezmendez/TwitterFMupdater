73024e53ce1cf11afd01a5d2f6c9f98d9fc4cc1d
cgomezmendez <cjgm23@gmail.com>
2012-11-29 19:44:26 +0000

    action listener y popup para menu item programa inicaido

--- src/com/controller/StartMenuController.java
+++ src/com/controller/StartMenuController.java
@@ -4,6 +4,9 @@
  */
 package com.controller;
 
+import com.Main;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
@@ -11,5 +14,12 @@ import java.awt.event.ActionListener;
  * @author cgomezmendez
  */
 public class StartMenuController implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        AppController app = new AppController();
+        app.guardarEstado(true);
+        Main.getTrayIcon().displayMessage("Info:", "Programa iniciado", TrayIcon.MessageType.INFO);
+    }
     
 }
